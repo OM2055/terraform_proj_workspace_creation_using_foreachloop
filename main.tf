f9ef5dab6106dca4423e41d8f5435a74c7328630
OM2055 <omkar.dba@gmail.com>
2024-09-16 14:30:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,31 +12,6 @@ provider "tfe" {
   token    = var.tfe_token  # Replace with your Terraform Cloud token
 }
 
-# Input variables for organization name and token
-variable "organization_name" {
-  type        = string
-  description = "The name of the organization in Terraform Cloud"
-}
-
-variable "tfe_token" {
-  type        = string
-  description = "The API token for Terraform Cloud"
-  sensitive   = true
-}
-
-variable "project_name" {
-  type        = string
-  description = "The name of the project to create"
-  default     = "TCS_Test01"  # Set the default project name
-}
-
-# List of workspace names
-variable "workspace_names" {
-  type        = list(string)
-  description = "The names of the workspaces to create"
-  default     = ["example-workspace-01", "example-workspace-02"]
-}
-
 # Get the organization details using a data source
 data "tfe_organization" "example" {
   name = var.organization_name
@@ -46,6 +21,13 @@ data "tfe_organization" "example" {
 resource "tfe_project" "example" {
   name         = var.project_name
   organization = var.organization_name
+}
+
+# List of workspace names
+variable "workspace_names" {
+  type        = list(string)
+  description = "The names of the workspaces to create"
+  default     = ["example-workspace-01", "example-workspace-02"]
 }
 
 # Create workspaces using for_each
